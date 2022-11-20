2999c488fb95605112f4f17c2b32bc2637ddd16d
pangyongliang <pang5572>
2022-11-20 13:01:56 +0800

    test

--- main/main.go
+++ main/main.go
@@ -7,13 +7,13 @@ import (
 )
 
 func main() {
-    var name = flag.String("name", "liang", "usage")
-    var name2 string
-    flag.StringVar(&name2, "name2", "yaoming", "usage of name")
-    flag.Parse()
-    fmt.Println("name: ", *name)
-    fmt.Println("name2: ", name2)
-    for index, arg := range os.Args {
-    	fmt.Printf("%d -> %s\n", index, arg)
+	var name = flag.String("name", "liang1", "usage")
+	var name2 string
+	flag.StringVar(&name2, "name2", "yaoming", "usage of name")
+	flag.Parse()
+	fmt.Println("name: ", *name)
+	fmt.Println("name2: ", name2)
+	for index, arg := range os.Args {
+		fmt.Printf("%d -> %s\n", index, arg)
 	}
 }
