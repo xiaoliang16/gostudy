7c8ce4a52bc43c214e5588ec3ef6fc36041eb7a2
pangyongliang <pang5572>
2022-11-20 12:59:08 +0800

    测试

---
+++ main/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+    var name = flag.String("name", "liang", "usage")
+    var name2 string
+    flag.StringVar(&name2, "name2", "yaoming", "usage of name")
+    flag.Parse()
+    fmt.Println("name: ", *name)
+    fmt.Println("name2: ", name2)
+    for index, arg := range os.Args {
+    	fmt.Printf("%d -> %s\n", index, arg)
+	}
+}
